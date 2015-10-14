dbb82d725319baa960b21848e1a7074b2ba55814
Brandon Block <branblock@gmail.com>
2015-10-14 13:34:29 -0700

    fixing issues with rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013015025) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20151008005154) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
