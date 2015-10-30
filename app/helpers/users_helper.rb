94d45a696282f3e0a2f9beea3a3068fcd17dbeb1
Brandon Block <branblock@gmail.com>
2015-10-30 12:29:49 -0700

    used helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def user_has_no_posts(user)
+    user = @user
+    @user.posts.count < 1
+  end
+
+  def user_has_no_comments(user)
+    user = @user
+    @user.comments.count < 1
+  end
 end
